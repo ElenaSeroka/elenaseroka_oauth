44e9d414ae01128266d459b9b055e451472fd2ba
Elena Seroka <pm222hs@student.lnu.se>
2022-05-05 11:09:22 +0200

    trying to fix app

--- src/routes/page-router.js
+++ src/routes/page-router.js
@@ -1,10 +1,9 @@
 import express from 'express'
 import { PageController } from '../controllers/page-controller.js'
-import { loggedIn } from '../middlewares/loggedIn.js'
 
 export const pageRouter = express.Router()
 export const controller = new PageController()
 
 pageRouter.get('/', controller.renderMain)
-pageRouter.get('/login', loggedIn, controller.renderLoginPage)
+pageRouter.get('/login', controller.renderLoginPage)
 pageRouter.get('/logout', controller.renderLogout, controller.logout)
